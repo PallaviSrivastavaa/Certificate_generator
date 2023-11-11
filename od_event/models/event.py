18eeae2a2d82e4ca48346a82c0a40310c5f3413d
shy <pallavisri0077@gmail.com>
2023-11-11 23:17:55 +0530

    working email functionality on super user

--- od_event/models/event.py
+++ od_event/models/event.py
@@ -7,6 +7,18 @@ class EventEvent(models.Model):
     event_id = fields.Many2one('event.event', string='event', required=True)
     partner_id = fields.Many2many(
         'res.partner', string='Recipients')
+    email = fields.Char( string='Email')
+    user_id = fields.Many2one(
+        'res.users', string='Responsible', tracking=True,
+        default=lambda self: self.env.user)
+    company_id = fields.Many2one(
+        'res.company', string='Company', change_default=True,
+        default=lambda self: self.env.company,
+        required=False)
+    organizer_id = fields.Many2one(
+        'res.partner', string='Organizer', tracking=True,
+        default=lambda self: self.env.company.partner_id,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
 
     def action_test(self):
         template = self.env.ref('od_event.certificate_mail_template', raise_if_not_found=False)
@@ -20,7 +32,7 @@ class EventEvent(models.Model):
             default_use_template=bool(template),
             default_template_id=template and template.id or False,
             default_email_layout_xmlid='mail.mail_notification_light',
-           # default_mailing_domain=repr([('event_id', 'in', self.ids), ('state', '!=', 'cancel')])
+            default_mailing_domain=repr([('event_id', 'in', self.ids), ('state', '!=', 'cancel')])
         )
         return {
             'type': 'ir.actions.act_window',
--- od_event/wizard/send_mail.py
+++ od_event/wizard/send_mail.py
@@ -15,22 +15,29 @@ class SendMail(models.TransientModel):
 
     event_id = fields.Many2one('event.event', string='event', required=True)
     partner_id = fields.Many2many('res.partner',string='Recipients')
-   
-
-
-   
-    
-
     emails = fields.Text(string='Additional emails', help="This list of emails of recipients will not be converted in contacts.\
         #Emails must be separated by commas, semicolons or newline.")
     mail_server_id = fields.Many2one('ir.mail_server', 'Outgoing mail server')
+    email_from = fields.Char(
+        'From', compute='_compute_email_from', readonly=False, store=True)
+    user_id = fields.Many2one(
+        'res.users', string='Responsible', tracking=True,
+        default=lambda self: self.env.user)
+    company_id = fields.Many2one(
+        'res.company', string='Company', change_default=True,
+        default=lambda self: self.env.company,
+        required=False)
+    organizer_id = fields.Many2one(
+        'res.partner', string='Organizer', tracking=True,
+        default=lambda self: self.env.company.partner_id,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+
     
 
     @api.model
     def _get_default_author(self):
+        print(self.env.user.partner_id)
         return self.env.user.partner_id
-    email_from = fields.Char(
-        'From', compute='_compute_email_from', readonly=False, store=True)
     author_id = fields.Many2one(
         'res.partner', 'Author', index=True,
         ondelete='set null', default=_get_default_author)
@@ -48,40 +55,31 @@ class SendMail(models.TransientModel):
     
     def send_mail(self):
         self.ensure_one()
-
         # Ensure there are selected partners or additional emails
         if not self.partner_id.ids and not self.emails:
-
             raise UserError(_("Please select at least one recipient or enter additional emails."))
-
         # Get the email addresses from the selected partners
         partner_emails = [partner.email for partner in self.partner_id if partner.email]
-
         # Split and format additional emails
         additional_emails = []
         for email in emails_split.split(self.emails or ''):
             email_formatted = tools.email_split_and_format(email)
             if email_formatted:
                 additional_emails.extend(email_formatted)
-
         # Combine all email addresses
         all_emails = partner_emails + additional_emails
-
-     
-     
-
-       
+        email_from = self.organizer_id.email_formatted
+        print(email_from)
         mail_template = self.env.ref('od_event.certificate_mail_template', )
-        for email in all_emails:
-          
+        for email in all_emails:        
             mail_values = {
                 'subject': 'Your Subject',
                 'email_to': email,
+                'email_from': email_from,
                 'body_html': 'Your HTML Body',
                 
-            }
+            }    
             mail_template.send_mail(self.id, force_send=True, email_values=mail_values)
-
         return {'type': 'ir.actions.act_window_close'}
 
 
